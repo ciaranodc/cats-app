0e187ae5a77c34565709bf5c92b25027328a66ca
Ciaran <ciaranodc@gmail.com>
2023-10-10 12:02:09 +0100

    Add proguard rule to prevent release version of app crashing

--- app/src/main/java/com/codc/cats/data/source/local/database/AppDatabase.kt
+++ app/src/main/java/com/codc/cats/data/source/local/database/AppDatabase.kt
@@ -9,7 +9,8 @@ import com.codc.cats.data.source.local.database.entity.RemoteKeysEntity
 
 @Database(
     entities = [ImageEntity::class, RemoteKeysEntity::class],
-    version = 1
+    version = 1,
+    exportSchema = false
 )
 abstract class AppDatabase : RoomDatabase() {
     abstract fun imageDao(): ImageDao
